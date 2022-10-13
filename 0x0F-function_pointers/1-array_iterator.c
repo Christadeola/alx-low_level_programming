78adf77bd18cd75754d1bc69bc185dc1527760d5
Christadeola <christadeola95@gmail.com>
2022-10-13 05:50:33 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,25 +1,20 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: array of integers
- * @size: size of array
- * @cmp: function pointer
- * Return: index of first element that matches with `cmp`, or -1 if none found
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
  */
-
-int int_index(int *array, int size, int (*cmp)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	if (array == NULL || action == NULL)
+		return;
 
-	if (size < 1 || array == NULL || cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	while (size-- > 0)
 	{
-		if (cmp(array[i]))
-			return (i);
+		action(*array);
+		array++;
 	}
-
-	return (-1);
 }
